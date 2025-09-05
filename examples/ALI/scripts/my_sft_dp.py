1017d9f1a3faaeb37d505bf28d1fb933127d44f8
Ali Shafique <alishafique3@gmail.com>
2025-09-05 20:08:19 +0000

    my_sft_dp.py added

--- examples/ALI/scripts/my_sft_dp.py
+++ examples/ALI/scripts/my_sft_dp.py
@@ -24,54 +24,74 @@
 """
 Train Gemma-3 on the Codeforces COTS dataset.
 
-accelerate launch --config_file examples/accelerate_configs/deepspeed_zero3.yaml examples/scripts/sft_gemma3.py
+accelerate launch --config_file examples/ALI/accelerate_configs/single_gpu.yaml examples/ALI/scripts/my_sft_dp.py
 """
 
+
 import os
-
-from datasets import load_dataset
-from transformers import AutoModelForImageTextToText
-
 from trl import SFTConfig, SFTTrainer
+from datasets import load_dataset
+
+trainer = SFTTrainer(
+    model="meta-llama/Llama-3.2-1B-Instruct",
+    args=SFTConfig(
+        output_dir="Llama-3.2-1B-Instruct",
+        chat_template_path="meta-llama/Llama-3.2-1B-Instruct",
+    ),
+    train_dataset=load_dataset("trl-lib/Capybara", split="train"),
+)
+trainer.train()
 
 
-# Enable logging in a Hugging Face Space
-os.environ.setdefault("TRACKIO_SPACE_ID", "trl-trackio")
 
 
-def main():
-    # Load dataset
-    train_dataset = load_dataset("open-r1/codeforces-cots", split="train")
-    train_dataset = train_dataset.remove_columns("prompt")
-
-    # Load model
-    model_id = "google/gemma-3-12b-it"
-    model = AutoModelForImageTextToText.from_pretrained(model_id, attn_implementation="eager")
-
-    # Train model
-    training_args = SFTConfig(
-        output_dir=f"{model_id}-codeforces-SFT",
-        bf16=True,
-        use_liger_kernel=True,
-        gradient_checkpointing=True,
-        gradient_checkpointing_kwargs={"use_reentrant": False},
-        max_length=8192,
-        per_device_train_batch_size=1,
-        gradient_accumulation_steps=8,
-        dataset_num_proc=32,
-        num_train_epochs=1,
-    )
-
-    trainer = SFTTrainer(
-        args=training_args,
-        model=model,
-        train_dataset=train_dataset,
-    )
-    trainer.train()
-
-    # Push to hub
-    trainer.push_to_hub(dataset_name="open-r1/codeforces-cots")
 
 
-if __name__ == "__main__":
-    main()
+# import os
+
+# from datasets import load_dataset
+# from transformers import AutoModelForImageTextToText
+
+# from trl import SFTConfig, SFTTrainer
+
+
+# # Enable logging in a Hugging Face Space
+# os.environ.setdefault("TRACKIO_SPACE_ID", "trl-trackio")
+
+
+# def main():
+#     # Load dataset
+#     train_dataset = load_dataset("open-r1/codeforces-cots", split="train")
+#     train_dataset = train_dataset.remove_columns("prompt")
+
+#     # Load model
+#     model_id = "google/gemma-3-12b-it"
+#     model = AutoModelForImageTextToText.from_pretrained(model_id, attn_implementation="eager")
+
+#     # Train model
+#     training_args = SFTConfig(
+#         output_dir=f"{model_id}-codeforces-SFT",
+#         bf16=True,
+#         use_liger_kernel=True,
+#         gradient_checkpointing=True,
+#         gradient_checkpointing_kwargs={"use_reentrant": False},
+#         max_length=8192,
+#         per_device_train_batch_size=1,
+#         gradient_accumulation_steps=8,
+#         dataset_num_proc=32,
+#         num_train_epochs=1,
+#     )
+
+#     trainer = SFTTrainer(
+#         args=training_args,
+#         model=model,
+#         train_dataset=train_dataset,
+#     )
+#     trainer.train()
+
+#     # Push to hub
+#     trainer.push_to_hub(dataset_name="open-r1/codeforces-cots")
+
+
+# if __name__ == "__main__":
+#     main()
